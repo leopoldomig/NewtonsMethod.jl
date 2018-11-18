4624fe22e5fd307572e38d420a820fb11a5f8847
Leopoldo Gutierre <leopoldo.mig@gmail.com>
2018-11-17 22:05:37 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using NewtonsMethod
 using Test
+using LinearAlgebra, Statistics, Compat, ForwardDiff
 
 @testset "NewtonsMethod.jl" begin
-    # Write your own tests here.
+    f1(x) = (x-1.0)^3
+    f1′(x) = 3.0*(x-1.0)^2
+    x1₀ = 0.1
+    @test newtonroot(f1, f1′, x₀ = x1₀) ≈ 1.0
+    @test newtonroot(f1, x₀ = x1₀) ≈ 1.0;
 end
