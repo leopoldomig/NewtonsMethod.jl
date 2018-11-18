d347d997dd175f221147225cdb3b38c3f3602a83
Leopoldo Gutierre <leopoldo.mig@gmail.com>
2018-11-17 22:23:52 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ using LinearAlgebra, Statistics, Compat, ForwardDiff
     f1(x) = (x-1.0)^3
     f1′(x) = 3.0*(x-1.0)^2
     x1₀ = 0.1
-    @test newtonroot(f1, f1′, x₀ = x1₀) ≈ 1.0
-    @test newtonroot(f1, x₀ = x1₀) ≈ 1.0;
+    @test newtonroot(f1, f1′, x₀ = x1₀).root ≈ 0.9999998168636032
+    @test newtonroot(f1, x₀ = x1₀).root ≈ 0.9999998168636032
 end
